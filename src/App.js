ac0654ea98b1eb260e50a250d5f4eae116e661a5
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-04 14:05:02 +0900

    feat: 로그인 박스 및 프로필 로고 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import imgLogo from './google-logo.png'
-
+import profile from './profile.png'
 function App() {
   return (
-  <div className='container'>
-    <img src={imgLogo} alt='logo'/>
+      <div className='container'>
+  <div className='header'>
+    <img className='logo' src={imgLogo} alt='logo'/>
     <p className='Content'>Google 계정으로 로그인</p>
   </div>
+    <div className='login-box'>
+      <img className='profile' src={profile}/>
+    </div>
+      </div>
 
   );
 }
